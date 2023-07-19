1595caf0a43c57f32fc0bc7aa188de8be7f2e38c
BlackJhossy <jhossymarbalderrama@gmail.com>
2023-07-19 16:22:41 -0300

    Seccion Listadod e Proyectos

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,34 +1,57 @@
 import { Component } from '@angular/core';
 import { Project } from 'src/app/models/project';
 import { ProjectService } from 'src/app/services/project.service';
-
+import { UploadService } from 'src/app/services/upload.service';
+import { Global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService, UploadService]
 })
 export class CreateComponent {
   public title: string;
   public project: Project;
+  public status: String = "";
+  public fileToUpload!: Array<File>;
 
   constructor(
-    private _projectService: ProjectService
+    private _projectService: ProjectService,
+    private _uploadService: UploadService
   ){
     this.title = "Crear Projecto";
     this.project = new Project('','','','',2019,'','');    
   }
 
-  onSubmit(){
+  onSubmit(projectForm: any){
     console.log(this.project);
+    //Guardar datos basicos
     this._projectService.saveProject(this.project).subscribe(
       response=>{
+        if(response.project )
+        {
+          
+          //Subir la imagen
+          this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id, [],this.fileToUpload,'image')
+          .then((result:any) =>{
+            this.status = 'success' ;
+            console.log(result);
+            projectForm.reset();
+          });
+
+        }
+        else this.status = 'faild'
         console.log(response);
       },
       error=>{
         console.log(<any>error);
       }
     );
+  }
+
+  fileChangeEvent(fileInput: any){
+    console.log(fileInput);
+    this.fileToUpload = <Array<File>>fileInput.target.files
   }
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Project } from 'src/app/models/project';
+import { ProjectService } from 'src/app/services/project.service';
+import { Global } from 'src/app/services/global';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectsComponent {
+  public projects!: Project[];
+  public url: string;
 
+  constructor(
+    private _projectService: ProjectService
+  ){
+    this.getProjects();
+    this.url = Global.url;
+  }
+
+  getProjects(){
+    this._projectService.getPojects().subscribe(
+      response =>{
+        console.log(response);
+        if(response.projects){
+          this.projects = response.projects;
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
 }
